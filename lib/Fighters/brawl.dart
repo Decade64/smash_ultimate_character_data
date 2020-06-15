2d9cd9ce8bf9256e581055642d37d165b2e49d9e
Decade64 <notadege@gmail.com>
2020-06-15 13:37:09 -0700

    Brawl and Smash 4 blanks

---
+++ lib/Fighters/brawl.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'FighterDefinition.dart';
+
+List<Fighter> brawl(){
+  List<Fighter> brawl = [];
+  //Meta Knight
+  brawl.add(Fighter(
+    name: Text("Meta Knight"),
+    fighterImage: Image.asset("lib/assets/brawl/MetaKnightImage.png"),
+    stockIconImageLocation: "assets/brawl/MetaKnightStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.blue,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/meta_knight.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Pit
+  brawl.add(Fighter(
+    name: Text("Pit and Dark Pit"),
+    fighterImage: Image.asset("lib/assets/brawl/PitImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/PitStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.grey,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/pit.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //ZSS
+  brawl.add(Fighter(
+    name: Text("Zero Suit Samus"),
+    fighterImage: Image.asset("lib/assets/brawl/ZSSImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/ZSSStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.blue,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/zero_suit_samus.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Wario
+  brawl.add(Fighter(
+    name: Text("Wario"),
+    fighterImage: Image.asset("lib/assets/brawl/WarioImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/WarioStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.yellow,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/wario.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Snake
+  brawl.add(Fighter(
+    name: Text("Snake"),
+    fighterImage: Image.asset("lib/assets/brawl/SnakeImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/SnakeStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.blueGrey,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/snake.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Ike
+  brawl.add(Fighter(
+    name: Text("Ike"),
+    fighterImage: Image.asset("lib/assets/brawl/IkeImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/IkeStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.orangeAccent,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/ike.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Pokemon Trainer
+  brawl.add(Fighter(
+    name: Text("Pokemon Trainer"),
+    fighterImage: Image.asset("lib/assets/brawl/PokémonTrainerImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/PTStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.yellow,
+    discordLink: "",
+    ultimateFrameDataLink: "",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Diddy Kong
+  brawl.add(Fighter(
+    name: Text("Diddy Kong"),
+    fighterImage: Image.asset("lib/assets/brawl/DiddyKongImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/DiddyKongStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.brown,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/diddy_kong.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Lucas
+  brawl.add(Fighter(
+    name: Text("Lucas"),
+    fighterImage: Image.asset("lib/assets/brawl/LucasImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/LucasStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.yellow,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/lucas.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Sonic
+  brawl.add(Fighter(
+    name: Text("Sonic"),
+    fighterImage: Image.asset("lib/assets/brawl/SonicImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/SonicStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.blue,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/sonic.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //King Dedede
+  brawl.add(Fighter(
+    name: Text("King Dedede"),
+    fighterImage: Image.asset("lib/assets/brawl/KingDededeImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/KingDededeStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.blue,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/king_dedede.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Olimar
+  brawl.add(Fighter(
+    name: Text("Olimar"),
+    fighterImage: Image.asset("lib/assets/brawl/OlimarImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/OlimarStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.yellow,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/olimar.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Lucario
+  brawl.add(Fighter(
+    name: Text("Lucario"),
+    fighterImage: Image.asset("lib/assets/brawl/LucarioImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/LucarioStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.blueGrey,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/lucario.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //ROB
+  brawl.add(Fighter(
+    name: Text("R.O.B"),
+    fighterImage: Image.asset("lib/assets/brawl/ROBImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/ROBStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.grey,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/rob.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  //Toon Link
+  brawl.add(Fighter(
+    name: Text("Toon Link"),
+    fighterImage: Image.asset("lib/assets/brawl/ToonLinkImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/ToonLinkStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.green,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/toon_link.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+  brawl.add(Fighter(
+    name: Text("Wolf"),
+    fighterImage: Image.asset("lib/assets/brawl/WolfImage.png"),
+    stockIconImageLocation: "lib/assets/brawl/WolfStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.purple,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/wolf.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: [],
+  ));
+}
---
+++ lib/Fighters/smash4.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'FighterDefinition.dart';
+
+List<Fighter> smashFour(){
+  List<Fighter> smashFour = [];
+  //Villager
+  smashFour.add(Fighter(
+    name: Text("Villager"),
+    fighterImage: Image.asset("lib/assets/smashFour/VillagerImage.png"),
+    stockIconImageLocation: "lib/assets/smashFour/VillagerStockIcon.png",
+    stockIconGraphOffset: Offset(0,0),
+    barColor: Colors.red,
+    discordLink: "",
+    ultimateFrameDataLink: "https://ultimateframedata.com/villager.php",
+    tacticsTitle: "",
+    tacticsBody: "",
+    additionalWidgets: [],
+    contributors: []
+  ));
+  //Mega Man
+  smashFour.add(Fighter(
+      name: Text("Mega Man"),
+      fighterImage: Image.asset("lib/assets/smashFour/MegaManImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/MegaManStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.blue,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/mega_man.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Wii Fit Trainer
+  smashFour.add(Fighter(
+      name: Text("Wii Fit Trainer"),
+      fighterImage: Image.asset("lib/assets/smashFour/WiiFitTrainerImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/WiiFitTrainerStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.grey,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/wii_fit_trainer.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Rosalina
+  smashFour.add(Fighter(
+      name: Text("Rosalina"),
+      fighterImage: Image.asset("lib/assets/smashFour/RosalinaImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/RosalinaStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.lightBlueAccent,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/rosalina_and_luma.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Little Mac
+  smashFour.add(Fighter(
+      name: Text("Little Mac"),
+      fighterImage: Image.asset("lib/assets/smashFour/LittleMacImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/LitleMacStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.green,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/little_mac.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Greninja
+  smashFour.add(Fighter(
+      name: Text("Greninja"),
+      fighterImage: Image.asset("lib/assets/smashFour/GreninjaImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/GreninjaStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.blueAccent,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/greninja.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Mii Brawler
+  smashFour.add(Fighter(
+      name: Text("Mii Brawler"),
+      fighterImage: Image.asset("lib/assets/smashFour/MiiBrawlerImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/MiiBrawlerStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.red,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/mii_brawler.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Mii Swordfighter
+  smashFour.add(Fighter(
+      name: Text("Mii Swordfighter"),
+      fighterImage: Image.asset("lib/assets/smashFour/MiiSwordfighterImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/MiiSwordfighterStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.blue,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/mii_swordfighter.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Mii Gunner
+  smashFour.add(Fighter(
+      name: Text("Mii Gunner"),
+      fighterImage: Image.asset("lib/assets/smashFour/MiiGunnerImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/MiiGunnerStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.orange,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/mii_gunner.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Palutena
+  smashFour.add(Fighter(
+      name: Text("Palutena"),
+      fighterImage: Image.asset("lib/assets/smashFour/PalutenaImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/PalutenaStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.lightGreen,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/palutena.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Pac-Man
+  smashFour.add(Fighter(
+      name: Text("Pac Man"),
+      fighterImage: Image.asset("lib/assets/smashFour/PacManImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/PacManStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.orangeAccent,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/pac_man.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Robin
+  smashFour.add(Fighter(
+      name: Text("Robin"),
+      fighterImage: Image.asset("lib/assets/smashFour/RobinImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/RobinStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.black,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/robin.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Shulk
+  smashFour.add(Fighter(
+      name: Text("Shulk"),
+      fighterImage: Image.asset("lib/assets/smashFour/ShulkImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/ShulkStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.red,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/shulk.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Bowser jr
+  smashFour.add(Fighter(
+      name: Text("Bowser Jr"),
+      fighterImage: Image.asset("lib/assets/smashFour/BowserJrImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/BowserJrStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.green,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/bowser_jr.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Duck Hunt
+  smashFour.add(Fighter(
+      name: Text("Bowser Jr"),
+      fighterImage: Image.asset("lib/assets/smashFour/DuckHuntImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/DuckHuntStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.brown,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/duck_hunt.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Ryu Ken
+  smashFour.add(Fighter(
+      name: Text("Ryu and Ken"),
+      fighterImage: Image.asset("lib/assets/smashFour/RyuImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/RyuStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.grey,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/ryu.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Cloud
+  smashFour.add(Fighter(
+      name: Text("Cloud"),
+      fighterImage: Image.asset("lib/assets/smashFour/CloudImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/CloudStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.blueGrey,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/cloud.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Corrin
+  smashFour.add(Fighter(
+      name: Text("Corrin"),
+      fighterImage: Image.asset("lib/assets/smashFour/CorrinImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/CorrinStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.white70,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/corrin.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+  //Bayonetta
+  smashFour.add(Fighter(
+      name: Text("Bayonetta"),
+      fighterImage: Image.asset("lib/assets/smashFour/BayoImage.png"),
+      stockIconImageLocation: "lib/assets/smashFour/BayoStockIcon.png",
+      stockIconGraphOffset: Offset(0,0),
+      barColor: Colors.black,
+      discordLink: "",
+      ultimateFrameDataLink: "https://ultimateframedata.com/bayonetta.php",
+      tacticsTitle: "",
+      tacticsBody: "",
+      additionalWidgets: [],
+      contributors: []
+  ));
+}
